535860a79861e673727faf73dc1d622d5a7d720d
Artur Vinícius <artur.t@escolar.ifrn.edu.br>
2022-05-16 15:41:52 -0300

    Update URI_2416.cs

--- Lista_03/URI_2416.cs
+++ Lista_03/URI_2416.cs
@@ -1,1 +1,16 @@
+using System; 
 
+public class Program {
+
+  public static void Main(string[] args) {
+    string[] n = Console.ReadLine().Split(' ');
+    int pista = int.Parse(n[0]);
+    int treino = int.Parse(n[1]);
+    if (pista > treino) 
+      { int pos = pista % treino;
+      Console.WriteLine(pos); }
+    else 
+      { int pos = treino % pista;
+      Console.WriteLine(pos); }
+  }
+}
