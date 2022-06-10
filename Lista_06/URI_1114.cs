788cb7aade198fdab6233368e6f12e7a3a65e1ad
Artur Vinícius <artur.t@escolar.ifrn.edu.br>
2022-06-10 15:52:41 -0300

    Create URI_1114.cs

---
+++ Lista_06/URI_1114.cs
@@ -0,0 +1,12 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+
+    while (Console.ReadLine() != "2002") {
+      Console.WriteLine("Senha Invalida");
+    }
+    Console.WriteLine("Acesso Permitido");
+  } 
+}
